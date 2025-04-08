aaf43ced6f7cfe4c0dd69e71e22ee885267c6b38
KrithikBavanendiran <bvino1522@gmail.com>
2025-04-08 20:17:54 +0100

    Lesson 16

---
+++ Shutdown.py
@@ -0,0 +1,16 @@
+def shutdown(apps):
+    return apps
+    
+apps=input("Are any apps open? Y or N: ")
+if apps=="Y":
+    print("Abort Shutdown")
+elif apps=="N":
+    print("Shutting Down")
+else:
+    print("Sorry input not recognised")
+
+    
+
+    
+
+
